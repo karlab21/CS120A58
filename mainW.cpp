896ccc3b8609a0c1edc817a3bd7cf0a85dbbc563
Kyuwoong Lee <kwooler@gmail.com>
2022-10-12 11:56:30 -0700

    Update and rename main.cpp to mainW.cpp

---
+++ mainW.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <fstream>
+
+
+using namespace std;
+
+int main()
+{
+    ofs.open("students.txt");
+    if (!ofs)
+    {
+        cout << "File Open Error\n";
+        exit(0);
+    }
+	
+	// Complete this code.
+	
+}
