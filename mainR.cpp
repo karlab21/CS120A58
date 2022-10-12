58d9b045ed8d307f2336bd45464a098922fc6b77
Kyuwoong Lee <kwooler@gmail.com>
2022-10-12 11:57:34 -0700

    Create mainR.cpp

---
+++ mainR.cpp
@@ -0,0 +1,21 @@
+#include    <iostream>
+#include    <fstream>
+using namespace std;
+
+int main()
+{
+    int     N;
+    string  stuName;
+    double  score1, score2, sum=0, avg;
+    double  tsum=0, tavg;
+    ifstream    ifs;
+
+    ifs.open("studetns.txt");
+    if ( !ifs ) {
+        cout << "File Open Error\n";
+        exit(0);
+    }
+  
+   /* Complete your code */
+  
+}
