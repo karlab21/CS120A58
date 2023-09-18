24b60957a58f01d89e01ae484439b2ed272d08e3
Kyu Lee <kwooler@gmail.com>
2023-09-17 18:37:55 -0700

    CS120A58

--- main_write.cpp
+++ main_write.cpp
@@ -9,21 +9,21 @@ int main()
    string stuName;
    ofstream ofs;
 
-   ofs.open("students.txt");
-   if (!ofs)
-   {
-      cout << "File Open Error\n";
-      exit(0);
-   }
-   cout << "Enter the total number of students:";
-   cin >> N;
-   ofs << N << endl;
+   // ofs.open("students.txt");
+   // if (!ofs)
+   // {
+   //    cout << "File Open Error\n";
+   //    exit(0);
+   // }
+   // cout << "Enter the total number of students:";
+   // cin >> N;
+   // ofs << N << endl;
 
-   for (int i = 0; i < N; i++)
-   {
-      cout << "Enter the student name and two scores: ";
-      cin >> stuName >> score1 >> score2;
-      ofs << stuName << "\t" << score1 << "\t" << score2 << endl;
-   }
-   ofs.close();
+   // for (int i = 0; i < N; i++)
+   // {
+   //    cout << "Enter the student name and two scores: ";
+   //    cin >> stuName >> score1 >> score2;
+   //    ofs << stuName << "\t" << score1 << "\t" << score2 << endl;
+   // }
+   // ofs.close();
 }
