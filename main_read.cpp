612902dbed08e3f30d7e141201ba3d6d4d9a0a75
karlab21 <karlabarragan03@icloud.com>
2024-09-03 12:11:18 -0700

    main

--- main_read.cpp
+++ main_read.cpp
@@ -4,7 +4,21 @@ using namespace std;
 
 int main()
 {
-	/***************************************************
-	 * Code your program here
-	 ************************************************** */
+	string sname;
+	int score1, score2;
+	int N, total;
+	double avg;
+
+	ifstream ifs;
+	ifs.open ("students.txt");
+	if (!ifs) {
+		cerr << "Error: open file \n";
+		exit (0);
+	}
+	ifs >> N;
+	for (int i=0; i=N;i++)
+
+		ifs >> sname >> score1 >>score2;
+		cout << "Students Name: " <<sname << "scores" << score1 << "\t" << score2 ;
+		cout << "\tTotal" << total << "\tAverage" << avg;
 }
--- main_write.cpp
+++ main_write.cpp
@@ -4,7 +4,23 @@ using namespace std;
 
 int main()
 {
-   /***************************************************
-    * Code your program here
-    ************************************************** */
+    int N;
+    string sname;
+    int score1, score2;
+    ofstream ofs;
+
+    ofs.open ("students.txt");
+    if (ofs){
+        cerr << "File Open Error \n";
+        exit(0);
+    }
+    cout << "Enter number of students: ";
+    cin >> N;
+    ofs << N << endl;
+    for(int i=0; i<N; i++)
+        cout << "Enter number of studet name: ";
+        cin >> sname;
+        cout << "Enter number of student's scores:";
+        cin >> score1 >> score2;
+        ofs << sname << "\t" << score1 << "\t" << score2;
 }
